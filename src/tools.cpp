d83be0246378c0edd929e8297441bb1a4ca563a7
cswksu <cswksu@gmail.com>
2019-01-26 23:06:24 -0500

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -27,7 +27,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	for (int i = 0; i < estimations.size(); ++i) {
 		VectorXd residual = estimations[i] - ground_truth[i];
 
-		residual = residual.arrya()*residual.array();
+		residual = residual.array()*residual.array();
 		rmse += residual;
 
 	}

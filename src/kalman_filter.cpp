f6e3a2bb02c6b4a0556d29c161107e494c83fe97
cswksu <cswksu@gmail.com>
2019-01-26 21:16:24 -0500

    1st commit

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,56 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+/* 
+ * Please note that the Eigen library does not initialize 
+ *   VectorXd or MatrixXd objects with zeros upon creation.
+ */
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+   * TODO: predict the state
+   */
+  VectorXd x_prime_ = F_ * x_;
+  MatrixXd P_prime_ = F_ * P_ * F_.transpose()+Q_;
+  
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Kalman Filter equations
+   */
+  VectorXd y_ = z-H_*x_prime;
+  MatrixXd S_ = H_ * P_prime_ * H_.transpose() + R_;
+  MatrixXd K_ = P_prime_ * H_.transpose() * S_.inverse();
+
+  x_=x_prime_ + K_ * y_;
+  MatrixXd kh_=K_*h_;
+  MatrixIXd iden_ = MatrixXD::Identity(kh_.rows(),kh_.cols());
+  P_=(iden_-kh_) * P_prime_;
+
+
+
+
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Extended Kalman Filter equations
+   */
+}
---
+++ src/tools.cpp
@@ -0,0 +1,50 @@
+#include "tools.h"
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+   * TODO: Calculate the RMSE here.
+   */
+  float rmse=0;
+  rmseV= VectorXd(estimations.size());
+  
+  for (int i=0; i<estimations.size(); ++i) {
+    rmse+=pow(estimations[i]-ground_truth(i),2);
+    rmseV[i)=pow(rmse/i,0.5);
+  
+  return rmseV;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+   * TODO:
+   * Calculate a Jacobian here.
+   */
+  jac=MatrixXd(3,4);
+  jac(0,0)=x_state(0)/pow(pow(x_state(0),2)+pow(x_state(1),2)),0.5);
+  jac(0,1)=x_state(1)/pow(pow(x_state(0),2)+pow(x_state(1),2)),0.5);
+  jac(0,2)=0;
+  jac(0,3)=0;
+  jac(1,0)=-x_state(1)/(pow(x_state(0),2)+pow(x_state(1),2));
+  jac(1,1)=x_state(0)/(pow(x_state(0),2)+pow(x_state(1),2));
+  jac(1,2)=0;
+  jac(1,3)=0;
+  jac(2,0)=x_state(1)*(x_state(2)*x_state(1)-x_state(3)*x_state(0))/pow(pow(x_state(0),2)+pow(x_state(1),2),1.5);
+  jac(2,1)=x_state(0)*(x_state(3)*x_state(0)-x_state(2)*x_state(1))/pow(pow(x_state(0),2)+pow(x_state(1),2),1.5);
+  jac(2,2)=x_state(0)/pow(pow(x_state(0),2)+pow(x_state(1),2)),0.5);
+  jac(2,3)=x_state(1)/pow(pow(x_state(0),2)+pow(x_state(1),2)),0.5);
+  
+  return jac;
+}
+

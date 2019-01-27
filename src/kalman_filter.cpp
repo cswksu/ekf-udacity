08705745a584bb2c2e438d60560d98aa57c93808
cswksu <cswksu@gmail.com>
2019-01-27 16:52:36 -0500

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -43,7 +43,7 @@ void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
-  std::cout << "H size: " << h.size() << std::endl;
+  std::cout << "H size: " << H_.size() << std::endl;
   std::cout << "Z size: " << z.size() << std::endl;
   VectorXd y_ = z-H_*x_prime_;
   std::cout << "y calculated" << std::endl;

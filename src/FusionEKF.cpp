394ba860a73f6f498d20c4d11337be834b65b6f3
cswksu <cswksu@gmail.com>
2019-01-26 23:17:57 -0500

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -78,9 +78,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       // TODO: Convert radar from polar to cartesian coordinates 
       //         and initialize state.
-      float rho=measurement_pack.raw_measurements(0);
-      float theta=measurement_pack.raw_measurements(1);
-      float ro_dot=measurement_pack.raw_measurements(2);
+      float rho=measurement_pack.raw_measurements_(0);
+      float theta=measurement_pack.raw_measurements_(1);
+      float ro_dot=measurement_pack.raw_measurements_(2);
 
       float x=rho*cos(theta);
       float y=rho*sin(theta);
@@ -93,8 +93,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       // TODO: Initialize state.
-      float x=measurement_pack.raw_measurements(0);
-      float y=measurement_pack.raw_measurements(1);
+      float x=measurement_pack.raw_measurements_(0);
+      float y=measurement_pack.raw_measurements_(1);
 
       ekf_.x_(0)=x;
       ekf_.x_(1)=y;

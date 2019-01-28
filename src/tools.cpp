4b0c86485d08b697d4eec2212a001b79952682dd
cswksu <cswksu@gmail.com>
2019-01-27 20:51:09 -0500

    streamline jacobian

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,64 +1,62 @@
 #include "tools.h"
 
-using Eigen::VectorXd;
+using Eigen::VectorXd; //using easier namespaces
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
+Tools::Tools() {} //constructor
 
-Tools::~Tools() {}
+Tools::~Tools() {} //destructor
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	const vector<VectorXd> &ground_truth) {
-	/**
-	 * TODO: Calculate the RMSE here.
-	 */
-	//std::cout << "calculating RMSE" << std::endl;
+	
+	//initialize rmse and set to zero
 	VectorXd rmse(4);
 	rmse << 0, 0, 0, 0;
 
+	//sanity check on estimation size and ground truth size
 	if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
-		return rmse;
+		return rmse; //return all zeros if non-sensical vector sizes
 	}
 
+	//for every estimation
 	for (int i = 0; i < estimations.size(); ++i) {
-		VectorXd residual = estimations[i] - ground_truth[i];
+		VectorXd residual = estimations[i] - ground_truth[i]; //calculate residual between estimation and ground truth
 
-		residual = residual.array()*residual.array();
-		rmse += residual;
+		residual = residual.array()*residual.array(); //element-wise square
+		rmse += residual;  //rmse becomes sum of all squared residuals
 
 	}
-	rmse = rmse / estimations.size();
-	rmse = rmse.array().sqrt();
-	//std::cout << "RMSE successfully calculated" << std::endl;
+	rmse = rmse / estimations.size(); //divide by number of estimations
+	rmse = rmse.array().sqrt();  //square root finishes RMSE
 	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
-	//std::cout << "calculating jacobian" << std::endl;
-   MatrixXd jac(3,4);
+	MatrixXd jac(3,4);
 
-  if (x_state(0) == 0 && x_state(1)==0) {
-	  std::cout << "Divide by zero error in Jacobian Function, returning zeros." << std::endl;
-	  jac = MatrixXd::Zero(3, 4);
+	if (x_state(0) == 0 && x_state(1)==0) {
+		std::cout << "Divide by zero error in Jacobian Function, returning zeros." << std::endl;
+		jac = MatrixXd::Zero(3, 4);
 	  
-  }
-  jac(0,0)=x_state(0)/pow(pow(x_state(0),2)+pow(x_state(1),2),0.5);
-  jac(0,1)=x_state(1)/pow(pow(x_state(0),2)+pow(x_state(1),2),0.5);
-  jac(0,2)=0;
-  jac(0,3)=0;
-  jac(1,0)=-x_state(1)/(pow(x_state(0),2)+pow(x_state(1),2));
-  jac(1,1)=x_state(0)/(pow(x_state(0),2)+pow(x_state(1),2));
-  jac(1,2)=0;
-  jac(1,3)=0;
-  jac(2,0)=x_state(1)*(x_state(2)*x_state(1)-x_state(3)*x_state(0))/pow(pow(x_state(0),2)+pow(x_state(1),2),1.5);
-  jac(2,1)=x_state(0)*(x_state(3)*x_state(0)-x_state(2)*x_state(1))/pow(pow(x_state(0),2)+pow(x_state(1),2),1.5);
-  jac(2,2)=x_state(0)/pow(pow(x_state(0),2)+pow(x_state(1),2),0.5);
-  jac(2,3)=x_state(1)/pow(pow(x_state(0),2)+pow(x_state(1),2),0.5);
+	}
+
+	float c1 = pow(x_state(0), 2) + pow(x_state(1), 2);
+	float c2 = pow(c1, 0.5);
+	float c3 = pow(pow(x_state(0), 2) + pow(x_state(1), 2), 1.5)
+	jac(0,0)=x_state(0)/c2;
+	jac(0,1)=x_state(1)/c2;
+	jac(0,2)=0;
+	jac(0,3)=0;
+	jac(1,0)=-x_state(1)/c1;
+	jac(1,1)=x_state(0)/c1;
+	jac(1,2)=0;
+	jac(1,3)=0;
+	jac(2,0)=x_state(1)*(x_state(2)*x_state(1)-x_state(3)*x_state(0))/c3;
+	jac(2,1)=x_state(0)*(x_state(3)*x_state(0)-x_state(2)*x_state(1))/c3;
+	jac(2,2)=x_state(0)/c2;
+	jac(2,3)=x_state(1)/c2;
   
   return jac;
 }
